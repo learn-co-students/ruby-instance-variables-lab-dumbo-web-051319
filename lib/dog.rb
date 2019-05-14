ca7d3e323db9249e6288b592706e73b37b94e30a
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-14 23:02:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,18 +13,3 @@ lassie.name = "Lassie"
 
 puts lassie.name 
 
-# class Dog
- 
-#   def name=(dogs_name)
-#     @this_dogs_name = dogs_name
-#   end
- 
-#   def name
-#     @this_dogs_name
-#   end
-# end
- 
-# lassie = Dog.new
-# lassie.name = "Lassie"
- 
-# puts lassie.name
